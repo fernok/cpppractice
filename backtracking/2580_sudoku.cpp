bdeb24eebea8789887c2e620fb4856f7100ec407
fernok <minkim918@gmail.com>
2020-07-15 16:36:14 +0900

    finished sudoku

--- backtracking/2580_sudoku.cpp
+++ backtracking/2580_sudoku.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <vector>
-
-bool flag = false;
 
 bool check_legit(int v[][9], int row, int col, int num) {
     int now_row = (row / 3) * 3;
@@ -15,23 +14,20 @@ bool check_legit(int v[][9], int row, int col, int num) {
 }
 
 void run_sudoku(int v[][9], std::vector<int> zero, int index) {
-    //printf("%d ", index);
-    if(flag) return;
     if(index == zero.size()) {
-        flag = true;
         for(int i = 0; i < 9; i++) {
             for(int j = 0; j < 9; j++) printf("%d ", v[i][j]);
             printf("\n");
         }
-        return;
+        exit(0);
     }
     int row = zero[index] / 9;
     int col = zero[index] % 9;
     for(int i = 1; i <= 9; i++) { 
-        if(flag) return;
         if(check_legit(v, row, col, i)) {
             v[row][col] = i;
             run_sudoku(v, zero, index + 1);
+            v[row][col] = 0;
         }
     }
 }
@@ -46,9 +42,5 @@ int main() {
         }
     }
     run_sudoku(sudoku, zero, 0);
-    for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) printf("%d ", sudoku[i][j]);
-            printf("\n");
-        }
     return 0;
 }
