5c3c0667bd42b60f70e9d4738881b6f1f17b7263
fernok <minkim918@gmail.com>
2020-07-18 23:45:22 +0900

    started stairs

--- dynamic_programming/2579_stairs.cpp
+++ dynamic_programming/2579_stairs.cpp
@@ -1,9 +1,29 @@
 #include <stdio.h>
 #include <vector>
 
+int n;
+std::vector<int> input;
+
+int get_max(int x, int y) { return x > y ? x : y; }
+
+void get_points(std::vector<int> &v, int n) {
+    if(n == 0) v[n] == input[0];
+    else if(n == 1) {
+
+    }
+    else if(n == 2) {
+        if(v[0] == 0) get_points(v, 0);
+        v[n] == v[0]
+    }
+}
+
 int main() {
-    int n;
     scanf("%d", &n);
-    std::vector<int> stairs;
+    int x;
+    std::vector<int> stairs(n, 0);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        input.push_back(x);
+    }
     return 0;
 }
