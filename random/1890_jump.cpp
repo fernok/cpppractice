7cd14063b65f70e5c13e6ef63ad32ebddea1273f
fernok <minkim918@gmail.com>
2020-08-10 15:16:15 +0900

    jump finished

--- random/1890_jump.cpp
+++ random/1890_jump.cpp
@@ -2,29 +2,31 @@
 
 using namespace std;
 
-int n;
-long long cnt = 0;
+int n, val;
 int board[100][100];
-
-void jump(int x, int y) {
-    if(x == n - 1 && y == n - 1) {
-        cnt++;
-        return;
-    }
-    if(board[x][y] == 0) return;
-    if(x + board[x][y] < n) jump(x + board[x][y], y);
-    if(y + board[x][y] < n) jump(x, y + board[x][y]);
-}
+long long route[100][100];
 
 int main() {
     cin >> n;
     for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) cin >> board[i][j];
+        for(int j = 0; j < n; j++) {
+            route[i][j] = 0;
+            cin >> board[i][j];
+        }
     }
 
-    jump(0, 0);
+    route[0][0] = 1;
 
-    cout << cnt << endl;
+    for(int i = 0; i < n; i ++) {
+        for(int j = 0; j < n; j++) {
+            if(board[i][j] == 0) continue;
+            val = board[i][j];
+            if(i + val < n) route[i + val][j] += route[i][j];
+            if(j + val < n) route[i][j + val] += route[i][j];
+        }
+    }
+
+    cout << route[n - 1][n - 1] << endl;
 
     return 0;
 }
