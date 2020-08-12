c5c1477ecf902eb69f7dae63da452a90f00ee0f2
fernok <minkim918@gmail.com>
2020-08-12 20:18:46 +0900

    candycandy finished

--- random/2878_candycandy.cpp
+++ random/2878_candycandy.cpp
@@ -1,13 +1,38 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <math.h>
 
 using namespace std;
 
+long long minimum(long long a, long long b) {
+    return a > b ? b : a;
+}
+
 int main() {
     long m;
-    int n;
+    long long sum = 0, given = 0, rage = 0;
+    long long leftover, democracy;
+    long long divider = pow(2, 64);
+    int n, i;
     cin >> m >> n;
-    vector<int> desire(n);
-    for(int i = 0; i < n; i++) cin >> desire[i];
+    vector<long> desire(n);
+    for(i = 0; i < n; i++) {
+        cin >> desire[i];
+        sum += (long long) desire[i];
+    }
+    sort(desire.begin(), desire.end());
+    leftover = sum - (long long) m;
+
+    for(i = 0; i < n; i++) {
+        if(leftover == 0) continue;
+        given = desire[i] < leftover / (n - i) ? desire[i] : leftover / (n - i);
+        leftover -= given;
+        rage += given * given;
+        rage %= divider;
+
+    }
+    
+    cout << rage % divider << endl;
     return 0;
 }
