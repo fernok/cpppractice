dcbbad1b31e23891bcc9b48fd93ca4ea92e65c2d
fernok <minkim918@gmail.com>
2020-08-12 18:35:06 +0900

    dice finished

--- random/1041_dice.cpp
+++ random/1041_dice.cpp
@@ -6,34 +6,28 @@ using namespace std;
 
 int dice[6];
 
-int get_two(int large) {
+int get_two() {
     int small = 51 * 2;
     for(int i = 0; i < 5; i++) {
-        if(i == large) continue;
         for(int j = i + 1; j < 6; j++) {
-            if(j == large) continue;
+            if(i + j == 5) continue;
             if(dice[i] + dice[j] < small) small = dice[i] + dice[j];
         }
     }
     return small;
 }
 
-int sum_three(int x, int y, int z) {
-    return dice[x] + dice[y] + dice[z];
-}
-
 int get_three() {
-    int small = 51 * 3;
-    if(sum_three(0, 1, 2) < small) small = sum_three(0, 1, 2);
-    if(sum_three(0, 1, 3) < small) small = sum_three(0, 1, 3);
-    if(sum_three(0, 2, 4) < small) small = sum_three(0, 2, 4);
-    if(sum_three(0, 3, 4) < small) small = sum_three(0, 3, 4);
-    if(sum_three(5, 1, 2) < small) small = sum_three(5, 1, 2);
-    if(sum_three(5, 2, 4) < small) small = sum_three(5, 2, 4);
-    if(sum_three(5, 3, 4) < small) small = sum_three(5, 3, 4);
-    if(sum_three(5, 1, 3) < small) small = sum_three(5, 1, 3);
+    int small = 51 * 2;
 
-    return small;
+    for(int i = 1; i < 4; i++) {
+        for(int j = i + 1; j < 5; j++) {
+            if(i + j == 5) continue;
+            if(dice[i] + dice[j] < small) small = dice[i] + dice[j];
+        }
+    }
+
+    return dice[0] > dice[5] ? dice[5] + small : dice[0] + small;
 
 }
 
@@ -41,11 +35,11 @@ int main() {
     int i;
     long long n;
     long long sum = 0;
-    int a, b, c, d, e, f;
-    int small = 51, large = 0, large_index = -1;
+    int small = 51;
     cin >> n;
     for(i = 0; i < 6; i++) {
         cin >> dice[i];
+        if(dice[i] < small) small = dice[i];
     }
 
     if(n == 1) {
@@ -54,16 +48,17 @@ int main() {
         cout << sum << endl;;
         return 0;
     }
-    int two = get_two(large_index);
+
+    int two = get_two();
     int three = get_three();
+
+    cout << two << " " << three << " " << small << endl;
 
     long long th3 = 4 * three;
     long long tw2 = two * (8 * n - 12);
     long long one = small * ((n - 2) * (n - 2) * 5 + (n - 2) * 4);
     sum = th3 + tw2 + one;
     cout << sum << endl;
-
-    // cout << 4 * three + 5 * small * (n - 2) * (n - 2) + 4 * small * (n - 2) + 8 * two * (n - 2) + two * 4 << endl;
 
     return 0;
 }
---
+++ random/2878_candycandy.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    long m;
+    int n;
+    cin >> m >> n;
+    vector<int> desire(n);
+    for(int i = 0; i < n; i++) cin >> desire[i];
+    return 0;
+}
