ab197e08cd11a1e9d4069754b53f8b28af5d4fe0
fernok <minkim918@gmail.com>
2021-09-17 23:43:23 +0900

    Start 3079

---
+++ samsung/3079.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+
+long long desk[100000];
+long long judge_time[100000];
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) cin >> judge_time[i];
+    
+
+    return 0;
+}
