5798434e68e1825b4ad95d91bd617eee3063704d
fernok <minkim918@gmail.com>
2021-09-18 00:19:20 +0900

    Finish 3079

--- samsung/3079.cpp
+++ samsung/3079.cpp
@@ -4,14 +4,42 @@ using namespace std;
 
 int n, m;
 
-long long desk[100000];
 long long judge_time[100000];
 
 int main() {
     cin >> n >> m;
 
-    for (int i = 0; i < n; i++) cin >> judge_time[i];
+    long long val = 0;
+    int i;
+
+    for (i = 0; i < n; i++) {
+        cin >> judge_time[i];
+        val = max(val, judge_time[i]);
+    }
     
+    long long left = 1;
+    long long right = val * m;
+    long long answer = 0;
+    long long people;
+    long long middle;
+
+    while (left <= right) {
+        middle = left + (right - left) / 2;
+        people = 0;
+
+        for (i = 0; i < n; i++) {
+            people += middle / judge_time[i];
+        }
+
+        if (people < m) {
+            left = middle + 1;
+        } else {
+            answer = middle;
+            right = middle - 1;
+        }
+    }
+
+    cout << answer << endl;
 
     return 0;
 }
